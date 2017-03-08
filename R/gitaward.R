d11b6e660c5ab4e9e0f7f87a29f976c79073389e
Guangchuang Yu <guangchuangyu@gmail.com>
2017-03-08 15:37:23 +0000

    version 0.0.2

---
+++ R/gitaward.R
@@ -0,0 +1,22 @@
+##' git award rank
+##'
+##'
+##' @title gitaward
+##' @param user user
+##' @param lang language
+##' @return rank
+##' @author Guangchuang Yu
+##' @export
+##' @examples
+##' gitaward('guangchuangyu', 'r')
+gitaward <- function(user="guangchuangyu", lang) {
+    url <- paste0("http://git-awards.com/users/", user)
+    x <- readLines(url)
+    language <- paste0("language=", lang, "\\W")
+    y <- x[grepl(language, x) & grepl("Worldwide", x)]
+    rank <- gsub(".*<strong>(\\d+)</strong>.*", "\\1", y)
+    total <- gsub(".*<strong>\\d+</strong>\\s/\\s([[:digit:][:space:]]+)<.*", "\\1", y)
+    total <- gsub("\\s", "", total)
+    res <- paste0(rank, "/", total)
+    return(res)
+}
